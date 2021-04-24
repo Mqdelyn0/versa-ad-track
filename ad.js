a5d0247d6cf5eba6eef4ce5db244bb9cf0a1ac08
kashike1 <actualronski@gmail.com>
2021-04-24 11:57:18 -0700

    Fix Crashes

--- ad.js
+++ ad.js
@@ -61,89 +61,66 @@ bot.on(`kicked`, (error) => {
 })
 
 bot.on('message', async(message) => {
-    let message_joined = '';
-    let server, rank, userr, user, type;
-    let json = message.json.extra;
-    type = 1;
-    if(!json) { 
-        json = message.json;
-        type = 2;
-    }
-    if(json) {
-        if(type) {
-            if(type === 1) {
-                let messages = json.length;
-                for(let num = 0; num < messages; num++) {
-                    message_joined = message_joined + json[num].text;
-                }
-            } else if(type === 2) {
-                message_joined = json.text;
-                while(message_joined.includes(`§`)) {
-                    for(let colour_code of color_codes) {
-                        message_joined = message_joined.replace(`${colour_code}`, ``);
-                    }
-                }
+    let ad_msg = message.toString();
+    let server, rank, userr, user;
+    ad_msg = ad_msg.replace(/(?!\w|\s)./g, '')
+    .replace(/\s+/g, ' ')
+    .replace(/^(\s*)([\W\w]*)(\b\s*$)/g, '$2');
+    if(!ad_msg) return;
+    if(ad_msg.toUpperCase().startsWith(`AD`)) {
+        let data = ad_msg.split(' ');
+        rank = data[1];
+        if(!valid_minehut_ranks.includes(rank)) {
+            rank = "DEFAULT";
+        }
+        userr = data[1];
+        if(valid_minehut_ranks.includes(rank)) {
+            userr = data[2];
+        }
+        user = userr.replace(':', '')
+        server = data[3];
+        if(server === "join") {
+            server = data[4];
+        }
+        console.log(`[Advertisement] ${user} has advertised ${server} with ${rank}!`);
+        if(server.toLowerCase() === "elestra" || server.toLowerCase() === "genversa" || server.toLowerCase() === "mineversa") {
+            let channel = dc_bot.channels.cache.get('832720733552902154');
+            let message_embed = new discord.MessageEmbed()
+                .setColor(`#FFBB33`)
+                .setDescription(`${user} has advertised!\n\nWhat Server? ${server.toLowerCase()}\nWhat Rank? ${rank}`)
+                .setThumbnail(`https://minotar.net/helm/${user}`)
+                .setAuthor(`Server Advertisement`)
+                .setFooter(`Coded by Madelyn | Versa Realms`)
+            channel.send(message_embed);
+            let model_1 = new advert_model({
+                advertiser: user,
+                server: server.toLowerCase(),
+                rank: rank,
+            });
+            try {
+                model_1.save();
+            } catch(error) {
+                console.log(error);
             }
-            message_joined = message_joined.replace(/(?!\w|\s)./g, '')
-                             .replace(/\s+/g, ' ')
-                             .replace(/^(\s*)([\W\w]*)(\b\s*$)/g, '$2');
-            if(!message_joined) return;
-            if(message_joined.toUpperCase().startsWith(`AD`)) {
-                let data = message_joined.split(' ');
-                rank = data[1];
-                if(!valid_minehut_ranks.includes(rank)) {
-                    rank = "DEFAULT";
+            let model_2 = await server_model.findOne({ server: server.toLowerCase() });
+            if(!model_2) {
+                let new_model_2 = new server_model({
+                    server: server.toLowerCase(),
+                    ad_party: 0
+                });
+                try {
+                    new_model_2.save();
+                } catch(error) {
+                    console.log(error);
                 }
-                userr = data[1];
-                if(valid_minehut_ranks.includes(rank)) {
-                    userr = data[2];
-                }
-                user = userr.replace(':', '')
-                server = data[3];
-                if(server === "join") {
-                    server = data[4];
-                }
-                console.log(`[Advertisement] ${user} has advertised ${server} with ${rank}!`);
-                if(server.toLowerCase() === "elestra" || server.toLowerCase() === "genversa" || server.toLowerCase() === "mineversa") {
-                    let channel = dc_bot.channels.cache.get('832720733552902154');
-                    let message_embed = new discord.MessageEmbed()
-                        .setColor(`#FFBB33`)
-                        .setDescription(`${user} has advertised!\n\nWhat Server? ${server.toLowerCase()}\nWhat Rank? ${rank}`)
-                        .setThumbnail(`https://minotar.net/helm/${user}`)
-                        .setAuthor(`Server Advertisement`)
-                        .setFooter(`Coded by Madelyn | Versa Realms`)
-                    channel.send(message_embed);
-                    let model_1 = new advert_model({
-                        advertiser: user,
-                        server: server.toLowerCase(),
-                        rank: rank,
-                    });
-                    try {
-                        model_1.save();
-                    } catch(error) {
+            } else if(model_2) {
+                let new_num = model_2.ad_party;
+                new_num++;
+                server_model.updateOne({ server: server.toLowerCase() }, { ad_party: new_num }, (error) => {
+                    if(error) {
                         console.log(error);
                     }
-                    let model_2 = await server_model.findOne({ server: server.toLowerCase() });
-                    if(!model_2) {
-                        let new_model_2 = new server_model({
-                            server: server.toLowerCase(),
-                            ad_party: 0
-                        });
-                        try {
-                            new_model_2.save();
-                        } catch(error) {
-                            console.log(error);
-                        }
-                    } else if(model_2) {
-                        let new_num = model_2.ad_party;
-                        new_num++;
-                        server_model.updateOne({ server: server.toLowerCase() }, { ad_party: new_num }, (error) => {
-                            if(error) {
-                                console.log(error);
-                            }
-                        });
-                    }
-                }
+                });
             }
         }
     }
